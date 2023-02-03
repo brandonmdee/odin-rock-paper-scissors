49ce054c7b677c6cdcdfd0605a55ba9114e6944f
brandonmdee <brandondickerson@cox.net>
2023-02-03 13:58:39 -0600

    Added index page and getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// rng to determine computerSelection
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+    
+}
+
+
