0515702380de020a56441e6ffb684be1ff9db21d
brandonmdee <brandondickerson@cox.net>
2023-02-13 10:52:50 -0600

    Added basic GUI functionality

--- javascript.js
+++ javascript.js
@@ -9,36 +9,53 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
+let result;
 let playerScore = 0;
 let computerScore = 0;
-    
+let playerSelection;
+let computerSelection;
+const buttons = document.querySelectorAll('#button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.className;
+        
+        console.log(playerSelection);
 
-// game runs the 5 round game of rock paper scissors and stops after 5 rounds.
+    })
+});
+
 function game() {
-    for (let i = 1; i <= 5; i++) {
-        const playerSelection = prompt("please choose 'rock', 'paper' or 'scissors'.");
-        const computerSelection = getComputerChoice();
-        console.log(`round ${i}, ${playRound(playerSelection,computerSelection)}`);
-        
-        
+    
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (playerScore === 5 || computerScore === 5) {
+        alert(`we have a winner. player has ${playerScore} points, computer has ${computerScore} points.`);
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+        alert(`${result}. player has ${playerScore} points, computer has ${computerScore} points.`);
     }
 }
+    
+const play = document.querySelector('#play');
+play.addEventListener('click', game);
+    
 
+//console.log(`${playRound(playerSelection,computerSelection)}`);
 // playRound takes the input from the game function (playerSelection and computerSelection) and figures out who wins each round by comparing.
 function playRound(playerSelection, computerSelection) {
-    let result;
-    playerSelection = playerSelection.toLowerCase();
+    
+    //playerSelection = playerSelection.toLowerCase();
        if ((playerSelection === "rock" || computerSelection === "rock") && (playerSelection === "scissors" || computerSelection === "scissors") ) {
         if (playerSelection ==="rock") {
             playerScore++;
-            return `player wins. rock beats scissors`;
+            result = `player wins. rock beats scissors`;
             
         } 
         else 
         {
             computerScore++;
-            return `computer wins. scissors beats rock`;
+            result = `computer wins. scissors beats rock`;
             
         }
        } 
@@ -46,43 +63,34 @@ function playRound(playerSelection, computerSelection) {
         else if ((playerSelection === "paper" || computerSelection === "paper") && (playerSelection === "rock" || computerSelection === "rock")) {
             if (playerSelection ==="paper") {
                 playerScore++;
-                return `player wins. paper beats rock`;
+                result = `player wins. paper beats rock`;
                 
             } 
             else {
                 computerScore++;
-                return `computer wins. paper beats rock`;
+                result = `computer wins. paper beats rock`;
                     
             }
         }
         else if ((playerSelection === "scissors" || computerSelection === "scissors") && (playerSelection === "paper" || computerSelection === "paper")) {
             if (playerSelection ==="scissors") {
                 playerScore++;
-                return `player wins. scissors beats paper`;
+                result = `player wins. scissors beats paper`;
                 
             } 
             else {
                 computerScore++;
-                return `computer wins. scissors beats paper`;
+                result = `computer wins. scissors beats paper`;
                 
             }
         }
         else if (playerSelection === computerSelection) {
-            return `DRAW.`
+            result = `DRAW.`
         }
        else {
-        return `invalid user entry. sorry you broke it`;
+        result = `invalid user entry. sorry you broke it`;
         
        }
        
-    }
+    };
 
-game(); //game runs
-//console logs the end result.
-if (playerScore > computerScore) {
-    console.log(`player has ${playerScore} points, computer has ${computerScore} points. player wins!`);
-} else if (computerScore > playerScore) {
-    console.log(`player has ${playerScore} points, computer has ${computerScore} points. computer wins!`);
-} else {
-    console.log(`player has ${playerScore} points, computer has ${computerScore} points. The game ends in a draw!`);
-}
